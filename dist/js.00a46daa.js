ab485d8e94db331a58e4489b643de80922de6a1e
aeeux <andreaseriksen99@hotmail.com>
2021-03-02 22:54:42 +0100

    Update
    Update

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -5812,7 +5812,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50762" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62101" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- dist/styles.8206c151.js
+++ dist/styles.8206c151.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\Users\\Andre\\Desktop\\portfolio2021\\images\\brush-stroke.png":[["brush-stroke.906cf266.png","images/brush-stroke.png"],"images/brush-stroke.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"sass/styles.scss":[function(require,module,exports) {
+},{"C:\\Users\\Andre\\Desktop\\websites\\Portfolio2021\\images\\brush-stroke.png":[["brush-stroke.906cf266.png","images/brush-stroke.png"],"images/brush-stroke.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"sass/styles.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -222,7 +222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50762" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62101" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
