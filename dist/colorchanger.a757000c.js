7c40ea050676d103798eb9a8ae0cf0c81656b1d8
aeeux <andreaseriksen99@hotmail.com>
2021-03-03 17:41:24 +0100

    update
    update

--- dist/colorchanger.a757000c.js
+++ dist/colorchanger.a757000c.js
@@ -117,84 +117,229 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"js/colorchanger.js":[function(require,module,exports) {
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+var cH;
+var cW;
+var bgColor = "#FF6138";
+var animations = [];
+var circles = [];
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+var colorPicker = function () {
+  var colors = ["#FF6138", "#FFBE53", "#2980B9", "#282741"];
+  var index = 0;
+
+  function next() {
+    index = index++ < colors.length - 1 ? index : 0;
+    return colors[index];
   }
 
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
+  function current() {
+    return colors[index];
   }
 
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
+  return {
+    next: next,
+    current: current
   };
+}();
 
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
+function removeAnimation(animation) {
+  var index = animations.indexOf(animation);
+  if (index > -1) animations.splice(index, 1);
 }
 
-var cssTimeout = null;
+function calcPageFillRadius(x, y) {
+  var l = Math.max(x - 0, cW - x);
+  var h = Math.max(y - 0, cH - y);
+  return Math.sqrt(Math.pow(l, 2) + Math.pow(h, 2));
+}
 
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
+function addClickListeners() {
+  document.addEventListener("touchstart", handleEvent);
+  document.addEventListener("mousedown", handleEvent);
+}
+
+;
+
+function handleEvent(e) {
+  if (e.touches) {
+    e.preventDefault();
+    e = e.touches[0];
   }
 
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
+  var currentColor = colorPicker.current();
+  var nextColor = colorPicker.next();
+  var targetR = calcPageFillRadius(e.pageX, e.pageY);
+  var rippleSize = Math.min(200, cW * .4);
+  var minCoverDuration = 750;
+  var pageFill = new Circle({
+    x: e.pageX,
+    y: e.pageY,
+    r: 0,
+    fill: nextColor
+  });
+  var fillAnimation = anime({
+    targets: pageFill,
+    r: targetR,
+    duration: Math.max(targetR / 2, minCoverDuration),
+    easing: "easeOutQuart",
+    complete: function complete() {
+      bgColor = pageFill.fill;
+      removeAnimation(fillAnimation);
     }
+  });
+  var ripple = new Circle({
+    x: e.pageX,
+    y: e.pageY,
+    r: 0,
+    fill: currentColor,
+    stroke: {
+      width: 3,
+      color: currentColor
+    },
+    opacity: 1
+  });
+  var rippleAnimation = anime({
+    targets: ripple,
+    r: rippleSize,
+    opacity: 0,
+    easing: "easeOutExpo",
+    duration: 900,
+    complete: removeAnimation
+  });
+  var particles = [];
 
-    cssTimeout = null;
-  }, 50);
+  for (var i = 0; i < 32; i++) {
+    var particle = new Circle({
+      x: e.pageX,
+      y: e.pageY,
+      fill: currentColor,
+      r: anime.random(24, 48)
+    });
+    particles.push(particle);
+  }
+
+  var particlesAnimation = anime({
+    targets: particles,
+    x: function x(particle) {
+      return particle.x + anime.random(rippleSize, -rippleSize);
+    },
+    y: function y(particle) {
+      return particle.y + anime.random(rippleSize * 1.15, -rippleSize * 1.15);
+    },
+    r: 0,
+    easing: "easeOutExpo",
+    duration: anime.random(1000, 1300),
+    complete: removeAnimation
+  });
+  animations.push(fillAnimation, rippleAnimation, particlesAnimation);
 }
 
-module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/components/menu.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
+function extend(a, b) {
+  for (var key in b) {
+    if (b.hasOwnProperty(key)) {
+      a[key] = b[key];
+    }
+  }
 
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"C:\\Users\\Andre\\Desktop\\websites\\Portfolio2021\\images\\brush-stroke.png":[["brush-stroke.906cf266.png","images/brush-stroke.png"],"images/brush-stroke.png"],"C:\\Users\\Andre\\Desktop\\websites\\Portfolio2021\\images\\hero.jpg":[["hero.0f0b5fd4.jpg","images/hero.jpg"],"images/hero.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"sass/styles.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
+  return a;
+}
 
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./components/menu.css":"sass/components/menu.css","C:\\Users\\Andre\\Desktop\\websites\\Portfolio2021\\images\\hero.jpg":[["hero.0f0b5fd4.jpg","images/hero.jpg"],"images/hero.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var Circle = function Circle(opts) {
+  extend(this, opts);
+};
+
+Circle.prototype.draw = function () {
+  ctx.globalAlpha = this.opacity || 1;
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+
+  if (this.stroke) {
+    ctx.strokeStyle = this.stroke.color;
+    ctx.lineWidth = this.stroke.width;
+    ctx.stroke();
+  }
+
+  if (this.fill) {
+    ctx.fillStyle = this.fill;
+    ctx.fill();
+  }
+
+  ctx.closePath();
+  ctx.globalAlpha = 1;
+};
+
+var animate = anime({
+  duration: Infinity,
+  update: function update() {
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, cW, cH);
+    animations.forEach(function (anim) {
+      anim.animatables.forEach(function (animatable) {
+        animatable.target.draw();
+      });
+    });
+  }
+});
+
+var resizeCanvas = function resizeCanvas() {
+  cW = window.innerWidth;
+  cH = window.innerHeight;
+  c.width = cW * devicePixelRatio;
+  c.height = cH * devicePixelRatio;
+  ctx.scale(devicePixelRatio, devicePixelRatio);
+};
+
+(function init() {
+  resizeCanvas();
+
+  if (window.CP) {
+    // CodePen's loop detection was causin' problems
+    // and I have no idea why, so...
+    window.CP.PenTimer.MAX_TIME_IN_LOOP_WO_EXIT = 6000;
+  }
+
+  window.addEventListener("resize", resizeCanvas);
+  addClickListeners();
+
+  if (!!window.location.pathname.match(/fullcpgrid/)) {
+    startFauxClicking();
+  }
+
+  handleInactiveUser();
+})();
+
+function handleInactiveUser() {
+  var inactive = setTimeout(function () {
+    fauxClick(cW / 2, cH / 2);
+  }, 2000);
+
+  function clearInactiveTimeout() {
+    clearTimeout(inactive);
+    document.removeEventListener("mousedown", clearInactiveTimeout);
+    document.removeEventListener("touchstart", clearInactiveTimeout);
+  }
+
+  document.addEventListener("mousedown", clearInactiveTimeout);
+  document.addEventListener("touchstart", clearInactiveTimeout);
+}
+
+function startFauxClicking() {
+  setTimeout(function () {
+    fauxClick(anime.random(cW * .2, cW * .8), anime.random(cH * .2, cH * .8));
+    startFauxClicking();
+  }, anime.random(200, 900));
+}
+
+function fauxClick(x, y) {
+  var fauxClick = new Event("mousedown");
+  fauxClick.pageX = x;
+  fauxClick.pageY = y;
+  document.dispatchEvent(fauxClick);
+}
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -398,5 +543,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/styles.8206c151.js.map
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/colorchanger.js"], null)
+//# sourceMappingURL=/colorchanger.a757000c.js.map
