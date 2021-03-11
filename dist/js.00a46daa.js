81e0c36ae2fc7b2c47b31539e84dcde758071e1e
aeeux <AndreasEriksen99@hotmail.com>
2021-03-11 17:26:16 +0100

    update
    update

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -5813,7 +5813,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56633" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56892" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
